257cc4f67fa49d68ccfb8ac9d6229c249ef922cf
Park Jangwon <vim@kakao.com>
2022-08-17 23:21:13 +0900

    :sparkles: add User API (find, find all, add)

--- user-api/src/main/java/in/parkjw/apps/user/contoller/UserAPI.java
+++ user-api/src/main/java/in/parkjw/apps/user/contoller/UserAPI.java
@@ -1,10 +1,16 @@
 package in.parkjw.apps.user.contoller;
 
+import in.parkjw.apps.user.dto.UserDTO;
+import in.parkjw.apps.user.entity.User;
 import in.parkjw.apps.user.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -17,5 +23,29 @@ public class UserAPI {
     public String test() {
 
         return userService.test();
+    }
+
+    @GetMapping(value = "/find/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> find(@PathVariable("id") @NotNull Long id) {
+        User result = userService.find(id);
+
+        return new ResponseEntity<>( result, HttpStatus.OK );
+    }
+
+    @GetMapping(value = "/findall", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<UserDTO.View>> findAll() {
+        List<UserDTO.View> result = userService.findAll();
+
+        return new ResponseEntity<>( result, HttpStatus.OK );
+    }
+
+    @PostMapping(value = "/add", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<UserDTO.View> add(@RequestBody UserDTO.Create dto) {
+
+        User user = dto.convert(User.class);
+        User saveUser = userService.add(user);
+        UserDTO.View view = saveUser.convert(UserDTO.View.class);
+
+        return new ResponseEntity<>(view, HttpStatus.CREATED);
     }
 }
--- user-api/src/main/java/in/parkjw/apps/user/service/UserService.java
+++ user-api/src/main/java/in/parkjw/apps/user/service/UserService.java
@@ -1,12 +1,39 @@
 package in.parkjw.apps.user.service;
 
+import in.parkjw.apps.core.mapper.MapperUtils;
+import in.parkjw.apps.user.dto.UserDTO;
+import in.parkjw.apps.user.entity.User;
+import in.parkjw.apps.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
+@RequiredArgsConstructor
 public class UserService {
+
+    final UserRepository userRepository;
 
     public String test() {
 
         return "Hello, User's API Test!";
+    }
+
+    public User find(Long id) {
+
+        return userRepository.findUserById(id);
+    }
+
+    public List<UserDTO.View> findAll() {
+        List<User> source = userRepository.findAll();
+
+        return MapperUtils.mapList(source, UserDTO.View.class);
+    }
+
+    public User add(User user) {
+        User result = userRepository.save(user);
+
+        return result;
     }
 }
